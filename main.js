c98f8fa56f93017e9bcbf60d7bf8539c7be6cc28
pauxiel <paul.scholes50@yahoo.com>
2020-09-28 18:00:16 -0700

    feat: added a search bar

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+
+const searchUser = document.getElementById('searchUser');
+
+
+
+
 const listRepos = async username => {
      const repos = await fetch(
          `https://api.github.com/users/${username}/repos?type=owner&sort=updated`
@@ -8,6 +14,7 @@ const listRepos = async username => {
      const markup = repos
        .map(
            repo => `
+           
                <li>
               <a href = "${repo.html_url}">${repo.name}</a>
                 (🌟 ${repo.stargazers_count})
@@ -20,9 +27,26 @@ const listRepos = async username => {
 
         content.innerHTML = `<ul>${markup}</ul>`;
 
+        
+        
+
 }
 
-listRepos('ObayuwanaPaul');
+
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+  
+    if(userText !== ''){
+     // Make http call
+     listRepos(userText)
+
+    } else {
+        content.innerHTML = '';
+    }
+  }); 
+
+// listRepos('ObayuwanaPaul');
 
 
 
